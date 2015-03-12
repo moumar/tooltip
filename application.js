f8829a20fbc5fb34a5692c36cabd4449f2be2b4e
Guillaume Pierronnet <guillaume@dataaddict.fr>
2015-03-12 22:03:14 +0100

    improved with test

--- application.js
+++ application.js
@@ -1,7 +1,6 @@
 var tooltip = document.querySelector('.tooltip');
 
-document.body.addEventListener('click', function(evt) {
+document.querySelector('.test-zone').addEventListener('mousemove', function(evt) {
   tooltip.style.top = evt.clientY + 'px';
   tooltip.style.left = evt.clientX + 'px';
-  console.log('click');
-}, true)
+}, false);
